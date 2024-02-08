1c44754cc592cfdc2c910668e566ed261a7f4b6f
Kevin Oliveira <kevinols08@gmail.com>
2024-02-07 21:52:21 -0300

    Check if group exists

--- backend/controllers/group.py
+++ backend/controllers/group.py
@@ -1,23 +1,25 @@
 from typing import List
-from fastapi import Depends
 from sqlmodel import Session, select
+from fastapi import HTTPException
+
 from models import Group, GroupRead, GroupCreate, GroupUpdate
-from db import get_session
 
 
-def show(session: Session = Depends(get_session)) -> List[GroupRead]:
+def show(session: Session) -> List[GroupRead]:
     groups = session.exec(select(Group)).all()
     return groups
 
 
-def index(group_id: int, session: Session = Depends(get_session)) -> GroupRead:
+def index(group_id: int, session: Session) -> GroupRead:
     group = session.get(Group, group_id)
-    return group
+    if group:
+        return group
+    raise HTTPException(status_code=404, detail="Group not found")
 
 
 def create(
         group_data: GroupCreate,
-        session: Session = Depends(get_session)) -> GroupRead:
+        session: Session) -> GroupRead:
     group = Group.model_validate(group_data)
     session.add(group)
     session.commit()
@@ -25,16 +27,17 @@ def create(
     return group
 
 
-def remove(group_id: int, session: Session = Depends(get_session)):
+def remove(group_id: int, session: Session):
     group = session.get(Group, group_id)
     if group:
         session.delete(group)
         session.commit()
-    return 204
+        return 204
+    raise HTTPException(status_code=404, detail="Group not found")
 
 
 def replace(group_id: int, group_data: GroupUpdate,
-            session: Session = Depends(get_session)) -> GroupRead:
+            session: Session) -> GroupRead:
     group = session.get(Group, group_id)
     if group:
         group_data = group_data.model_dump(exclude_unset=True)
@@ -43,10 +46,11 @@ def replace(group_id: int, group_data: GroupUpdate,
         session.add(group)
         session.commit()
         session.refresh(group)
-    return group
+        return group
+    raise HTTPException(status_code=404, detail="Group not found")
 
 
 def update(group_id: int, group_data: GroupUpdate,
-           session: Session = Depends(get_session)) -> GroupRead:
+           session: Session) -> GroupRead:
 
     pass
--- backend/routes/group.py
+++ backend/routes/group.py
@@ -1,5 +1,7 @@
 from typing import List
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
+from sqlmodel import Session
+from db import get_session
 
 from models import GroupRead, GroupCreate, GroupUpdate
 
@@ -9,30 +11,30 @@ router = APIRouter(prefix='/groups', tags=['groups'])
 
 
 @router.get('/')
-def fetch_groups() -> List[GroupRead]:
-    return group.show()
+def fetch_groups(session: Session = Depends(get_session)) -> List[GroupRead]:
+    return group.show(session)
 
 
 @router.get('/{group_id}')
-def fetch_group(group_id: int) -> GroupRead:
-    return group.index(group_id)
+def fetch_group(group_id: int, session: Session = Depends(get_session)) -> GroupRead:
+    return group.index(group_id, session)
 
 
 @router.post('', status_code=201)
-def create_group(group_data: GroupCreate) -> GroupRead:
-    return group.create(group_data)
+def create_group(group_data: GroupCreate, session: Session = Depends(get_session)) -> GroupRead:
+    return group.create(group_data, session)
 
 
 @router.delete('/{group_id}', status_code=204)
-def remove_group(group_id: int):
-    return group.remove(group_id)
+def remove_group(group_id: int, session: Session = Depends(get_session)):
+    return group.remove(group_id, session)
 
 
 @router.put('/{group_id}')
-def replace_group(group_id: int, group_data: GroupUpdate) -> GroupRead:
-    return group.replace(group_id, group_data)
+def replace_group(group_id: int, group_data: GroupUpdate, session: Session = Depends(get_session)) -> GroupRead:
+    return group.replace(group_id, group_data, session)
 
 
 @router.patch('/{group_id}')
-def update_group(group_id: int, group_data: GroupUpdate) -> GroupRead:
-    return group.update(group_id, group_data)
+def update_group(group_id: int, group_data: GroupUpdate, session: Session = Depends(get_session)) -> GroupRead:
+    return group.update(group_id, group_data, session)
