d32e209f7af9b11ec61963b7b9388fd02def2a2c
Kevin Oliveira <kevinols08@gmail.com>
2024-04-30 17:32:11 -0300

    Remove test data

--- frontend/src/components/SportsEventDetails.vue
+++ frontend/src/components/SportsEventDetails.vue
@@ -49,12 +49,9 @@
 <script setup>
 import { onMounted } from 'vue';
 import { ref } from 'vue';
-import { useRoute, useRouter } from 'vue-router'
 import { formatRelative, isAfter, isWithinInterval, parseISO } from 'date-fns'
 import Rate from './Rate.vue';
 import { computed } from 'vue';
-
-const route = useRoute()
 
 const playersHeaders = [
     { value: 'name', title: 'Nome' },
@@ -75,135 +72,6 @@ const selectedItem = ref({})
 const tab = ref(null)
 
 const getSportsEventDetails = () => {
-    sportsEvent.value = {
-        id: 3,
-        event_date: '2024-02-02 20:30:00.000',
-        voting_until: '2024-02-08 23:59:59.999',
-        checkin_from: '2024-02-02 08:00:00.000',
-        checkin_until: '2024-02-08 19:30:00.000',
-        teams_drawn: true,
-        players: [
-            {
-                "id": 1,
-                "name": "Lucas",
-                "rating": 4.12,
-                "rated": true
-            },
-            {
-                "id": 2,
-                "name": "Miguel",
-                "rating": 3.59,
-                "rated": false
-            },
-            {
-                "id": 3,
-                "name": "Gabriel",
-                "rating": 4.73,
-                "rated": true
-            },
-            {
-                "id": 4,
-                "name": "Arthur",
-                "rating": 2.68,
-                "rated": false
-            },
-            {
-                "id": 5,
-                "name": "Pedro",
-                "rating": 3.45,
-                "rated": true
-            },
-            {
-                "id": 6,
-                "name": "Mateus",
-                "rating": 4.56,
-                "rated": false
-            },
-            {
-                "id": 7,
-                "name": "Rafael",
-                "rating": 3.92,
-                "rated": true
-            },
-            {
-                "id": 8,
-                "name": "Felipe",
-                "rating": 2.87,
-                "rated": false
-            },
-            {
-                "id": 9,
-                "name": "Gustavo",
-                "rating": 3.14,
-                "rated": true
-            },
-            {
-                "id": 10,
-                "name": "Guilherme",
-                "rating": 4.22,
-                "rated": false
-            },
-            {
-                "id": 11,
-                "name": "Leonardo",
-                "rating": 3.88,
-                "rated": true
-            },
-            {
-                "id": 12,
-                "name": "Eduardo",
-                "rating": 2.95,
-                "rated": false
-            },
-            {
-                "id": 13,
-                "name": "Murilo",
-                "rating": 4.01,
-                "rated": true
-            },
-            {
-                "id": 14,
-                "name": "Henrique",
-                "rating": 3.67,
-                "rated": false
-            },
-            {
-                "id": 15,
-                "name": "Bruno",
-                "rating": 2.77,
-                "rated": true
-            }
-        ]
-    }
-    teams.value = [
-        {
-            'id': 0, 'players': [
-                { 'id': 12, 'name': 'Eduardo', 'rating': 2.95 },
-                { 'id': 6, 'name': 'Mateus', 'rating': 4.56 },
-                { 'id': 1, 'name': 'Lucas', 'rating': 4.12 },
-                { 'id': 14, 'name': 'Henrique', 'rating': 3.67 },
-                { 'id': 5, 'name': 'Pedro', 'rating': 3.45 },
-            ]
-        },
-        {
-            'id': 1, 'players': [
-                { 'id': 8, 'name': 'Felipe', 'rating': 2.87 },
-                { 'id': 3, 'name': 'Gabriel', 'rating': 4.73 },
-                { 'id': 13, 'name': 'Murilo', 'rating': 4.01 },
-                { 'id': 11, 'name': 'Leonardo', 'rating': 3.88 },
-                { 'id': 9, 'name': 'Gustavo', 'rating': 3.14 },
-            ]
-        },
-        {
-            'id': 2, 'players': [
-                { 'id': 15, 'name': 'Bruno', 'rating': 2.77 },
-                { 'id': 4, 'name': 'Arthur', 'rating': 2.68 },
-                { 'id': 10, 'name': 'Guilherme', 'rating': 4.22 },
-                { 'id': 7, 'name': 'Rafael', 'rating': 3.92 },
-                { 'id': 2, 'name': 'Miguel', 'rating': 3.59 },
-            ]
-        }
-    ]
 }
 
 const teamRating = (players) => {
