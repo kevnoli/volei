c8ff512fe9badf1171a7cea0a94db517976a5ae6
Kevin Oliveira <kevinols08@gmail.com>
2024-04-30 15:49:42 -0300

    Move container wrapping content

--- frontend/src/components/SportsEventDetails.vue
+++ frontend/src/components/SportsEventDetails.vue
@@ -1,48 +1,41 @@
 <template>
-    <v-container>
-        <v-row justify="center">
-            <v-col cols="12" sm="8" md="6">
-                <v-card>
-                    <v-card-title>
-                        <v-container>
-                            <v-row>
-                                <v-col>
-                                    {{ sportsEventName }}
-                                </v-col>
-                            </v-row>
-                        </v-container>
-                    </v-card-title>
-                    <v-card-text>
-                        <v-tabs v-model="tab">
-                            <v-tab value="players">Jogadores</v-tab>
-                            <v-tab value="teams">Times</v-tab>
-                        </v-tabs>
-                        <v-window v-model="tab">
-                            <v-window-item value="players">
-                                <v-data-table :headers="playersHeaders" :items="sportsEvent.players" :sort-by="sortBy"
-                                    @click:row="openRating">
-                                    <template #item.rated="{ item }">
-                                        <v-icon :icon="item.rated ? 'mdi-check' : 'mdi-close'"></v-icon>
-                                    </template>
-                                </v-data-table>
-                                <rate :item="selectedItem" :votingOpen="isAfter(sportsEvent.voting_until, new Date())"
-                                    :show-dialog="dialog" @update:showDialog="dialog = $event" />
-                            </v-window-item>
-                            <v-window-item value="teams">
-                                <v-list>
-                                    <div v-for="team in teams">
-                                        <v-list-subheader
-                                            :title="`Time ${team.players[0].name} (média ${teamRating(team.players)})`" />
-                                        <v-list-item v-for="player in team.players" :title="player.name" />
-                                        <v-divider></v-divider>
-                                    </div>
-                                </v-list>
-                            </v-window-item>
-                        </v-window>
-                    </v-card-text>
-                </v-card>
-            </v-col>
-        </v-row>
+    <v-card>
+        <v-card-title>
+            <v-container>
+                <v-row>
+                    <v-col>
+                        {{ sportsEventName }}
+                    </v-col>
+                </v-row>
+            </v-container>
+        </v-card-title>
+        <v-card-text>
+            <v-tabs v-model="tab">
+                <v-tab value="players">Jogadores</v-tab>
+                <v-tab value="teams">Times</v-tab>
+            </v-tabs>
+            <v-window v-model="tab">
+                <v-window-item value="players">
+                    <v-data-table :headers="playersHeaders" :items="sportsEvent.players" :sort-by="sortBy"
+                        @click:row="openRating">
+                        <template #item.rated="{ item }">
+                            <v-icon :icon="item.rated ? 'mdi-check' : 'mdi-close'"></v-icon>
+                        </template>
+                    </v-data-table>
+                    <rate :item="selectedItem" :votingOpen="isAfter(sportsEvent.voting_until, new Date())"
+                        :show-dialog="dialog" @update:showDialog="dialog = $event" />
+                </v-window-item>
+                <v-window-item value="teams">
+                    <v-list>
+                        <div v-for="team in teams">
+                            <v-list-subheader :title="`Time ${team.players[0].name} (média ${teamRating(team.players)})`" />
+                            <v-list-item v-for="player in team.players" :title="player.name" />
+                            <v-divider></v-divider>
+                        </div>
+                    </v-list>
+                </v-window-item>
+            </v-window>
+        </v-card-text>
         <div class="text-end pointer-events-none" style="position: fixed; bottom: 0; right: 0; margin: 16px;">
             <v-fab-transition>
                 <v-btn
@@ -51,7 +44,7 @@
             </v-fab-transition>
         </div>
         <v-layout-item class="text-end pointer-events-none" model-value position="bottom" size="30" />
-    </v-container>
+    </v-card>
 </template>
 <script setup>
 import { onMounted } from 'vue';
--- frontend/src/layouts/default/View.vue
+++ frontend/src/layouts/default/View.vue
@@ -1,8 +1,10 @@
 <template>
     <v-main>
-        <v-container class="justify-center">
-            <v-row>
-                <router-view />
+        <v-container>
+            <v-row justify="center">
+                <v-col cols="12" sm="10" md="8">
+                    <router-view />
+                </v-col>
             </v-row>
         </v-container>
     </v-main>
