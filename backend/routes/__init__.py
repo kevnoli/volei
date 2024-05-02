d457dd165e972e1fa8093aa5ed1a14226dd25dc7
Kevin Oliveira <kevinols08@gmail.com>
2024-05-02 16:33:55 -0300

    Fix schedule model

--- backend/routes/__init__.py
+++ backend/routes/__init__.py
@@ -1,8 +1,10 @@
 from fastapi import APIRouter
 from .event import router as event_router
 from .player import router as player_router
+from .settings import router as settings_router
 
 router = APIRouter()
 
 router.include_router(event_router)
 router.include_router(player_router)
+router.include_router(settings_router)
