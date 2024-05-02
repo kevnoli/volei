00866bfad333aebffc5de058fef2065a8f1850ac
Kevin Oliveira <kevinols08@gmail.com>
2024-05-02 14:27:45 -0300

    Revert "Add schedule table and routes"
    This reverts commit 87da238b16817939ce6a934422ef4c7a90b2b2ea.

--- backend/models/__init__.py
+++ backend/models/__init__.py
@@ -3,7 +3,6 @@ from .event import Event, EventRead, EventCreate, EventUpdate
 from .team import Team, TeamRead, TeamCreate
 from .event_player import EventPlayer
 from .team_player import TeamPlayer
-from .schedule import Schedule, ScheduleRead, ScheduleCreate, ScheduleUpdate
 
 __all__ = [
     "Player",
@@ -19,8 +18,4 @@ __all__ = [
     "TeamCreate",
     "EventPlayer",
     "TeamPlayer",
-    "Schedule",
-    "ScheduleRead",
-    "ScheduleCreate",
-    "ScheduleUpdate"
 ]
